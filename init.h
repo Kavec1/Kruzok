011c33a64031446b30e993a3ea7f34aaef97b74a
Kavec1 <matus.kavec@gmail.com>
2017-02-21 21:27:24 +0100

    Update init.h

--- init.h
+++ init.h
@@ -5,13 +5,6 @@
 #define F_CPU	8000000UL	//definicia frekvencie CPU
 #endif
 
-#define sbi(x,y) x |= _BV(y)	//set bit- nastavenie bitu
-#define cbi(x,y) x &= (~(_BV(y)))	//clear bit - nulovanie bitu
-#define tbi(x,y) x ^=_BV(y)		//toggle bit - zmena bitu
-
-#define is_high(y) (PINB & _BV(y) == _BV(y)) //=nestlačené kontrola, ci je nastaveny y-ty bit registra 'x'
-#define is_low(y) ((PINB & _BV(y)) != _BV(y)) //=stlačené kontrola, ci je vstupny pin nastaveny na logicku nulu
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
