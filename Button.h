af05a514cf77f9dadb2a510e80c1fdfb147ea91e
Kavec1 <matus.kavec@gmail.com>
2017-03-04 11:31:06 +0100

    Update Button.h

--- Button.h
+++ Button.h
@@ -5,41 +5,5 @@
 #define is_low(y) ((PINB & _BV(y)) != _BV(y)) //=stlačené kontrola, ci je vstupny pin nastaveny na logicku nulu
 void setButton(char x,int,y);  
 int Pin_state();
-  
-void setButton(char x,int y)
-{
-	switch(x)
-    {
-        case 'A':
-            DDRA |=(0<<y);
-            PORTA |=(1<<y);
-            break;
 
-        case 'B':
-            DDRB |=(0<<y);
-            PORTB |=(1<<y);
-            break;
-
-        case 'C':
-            DDRC |=(0<<y);
-            PORTC |=(1<<y);
-            break;
-
-        case 'D':
-            DDRD |=(0<<y);
-            PORTD |=(1<<y);
-            break;       
-    }
-}
-int Pin_state()
-{
-  if (is_low(PB2/3)
-  {
-     if (is_high(PB2/3))
-	  {
-		 return 1;
-	  }
-  }
-  return 0;
-}
 #endif /* Button_H_ */
