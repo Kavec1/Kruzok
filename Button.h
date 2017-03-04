7d4181226a1d3a5406dd175145b07933739f23b0
Kavec1 <matus.kavec@gmail.com>
2017-03-04 11:35:35 +0100

    Update Button.h

--- Button.h
+++ Button.h
@@ -4,7 +4,7 @@
 
 #define is_high(y) (PINA & _BV(y) == _BV(y)) //=nestlačené kontrola, ci je nastaveny y-ty bit registra 'x'
 #define is_low(y) ((PINA & _BV(y)) != _BV(y)) //=stlačené kontrola, ci je vstupny pin nastaveny na logicku nulu
-void setButton(char port,int pin);
-int Pin_state(char x,int y);
+void setButton(char port,short pin);
+int Pin_state(char x,short y);
 
 #endif /* Button_H_ */
